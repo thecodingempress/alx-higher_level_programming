f299f6bfeb9386a0d046ada989a74682cd8315aa
codyempress <codyempress@gmail.com>
2024-01-03 18:07:41 +0300

    caps

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def uppercase(str):
     for i in str:
 
@@ -6,4 +7,5 @@ def uppercase(str):
             print("{}".format(chr(new)), end="")
         else:
             print("{}".format(i), end="")
+    
     print("\n")
