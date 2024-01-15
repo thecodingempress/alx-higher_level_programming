916d31d781888358e09341e4baa668f1a0f64ce8
codyempress <codyempress@gmail.com>
2024-01-15 16:14:39 +0300

    Lists

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+
+    if idx < 0 or idx > (len(my_list)):
+        return None
+
+    for i in my_list:
+        if i == idx:
+           return (i)
