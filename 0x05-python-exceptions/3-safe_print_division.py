b8567fbcb01976d848739a842925edbd6df72353
codyempress <codyempress@gmail.com>
2024-01-22 20:14:24 +0300

    Using finally

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        res = a / b
+    except Exception:
+        res = None
+        return None
+    finally:
+        print("Inside result: {}".format(res))
+    
+    return res
