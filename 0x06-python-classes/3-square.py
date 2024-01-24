fd16367b8dc624b108d5196af390e3a1cb56af01
codyempress <codyempress@gmail.com>
2024-01-24 21:17:39 +0300

    Calculating the square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This is a class square that instantiates a private attribute with additional
+checks for logic
+"""
+
+class Square:
+    """
+    The class Square intitializes an instance with a private attribute
+    and checks the type and value
+
+    Attributes:
+        size: private instance that is checked for correct
+        type and value
+    """
+
+    def __init__(self, size=0):
+        """
+        This instance initializes an on object with the square size a private instance
+        attribute. It check if teh size os of teh correct type and value
+        """
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        Returns the area of the square
+        """
+        return self.__size ** 2
