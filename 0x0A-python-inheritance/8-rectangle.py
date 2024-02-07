f25e0f7733d920a0b4d3e16f5a6ece61838a411a
codyempress <codyempress@gmail.com>
2024-02-07 19:48:21 +0300

    edits

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+class inherits from baseGeometry
+
+"""
+class Rectange(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
